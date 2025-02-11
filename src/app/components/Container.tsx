67ea568b47952a5eea75fce22bcaf1366613c5c0
Saud Kadiri <saudkadiri@gmail.com>
2025-02-11 23:24:49 +0530

    container will be glass

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 const Container = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
-    <main className='p-2 inset-0 relative bg-[#fff] dark:bg-[#0F1014] text-black/90 dark:text-white/90 min-h-screen'>
+    <main className='bg-white/30 backdrop-blur-none'>
         {children}
     </main>
   )
