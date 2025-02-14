2db6122137ad43339b388b0f8605e4ff40b4f223
Saud Kadiri <saudkadiri@gmail.com>
2025-02-14 23:55:54 +0530

    containers

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,10 +1,14 @@
+import Container from '@/app/components/Container'
 import Page from '@/app/components/Page'
 import React from 'react'
 
 const Project = () => {
   return (
     <Page>
+      <Container>
+
       <div>This is a Project</div>
+      </Container>
     </Page>
   )
 }
